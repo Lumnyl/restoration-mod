18cbb1cfc4a6c04e8bc6e140335c60806861c4d6
lumnyl <scuram@hotmail.fr>
2025-06-19 19:07:07 +0200

    gagaga

--- lua/sc/tweak_data/weapontweakdata.lua
+++ lua/sc/tweak_data/weapontweakdata.lua
@@ -5693,7 +5693,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 						self.czech.has_description = true
 						self.czech.desc_id = "bm_czech_sc_desc"
 						self.czech.CLIP_AMMO_MAX = 18
-						self.czech.AMMO_MAX = 90
+						self.czech.AMMO_MAX = 180
 						self.czech.fire_mode_data.fire_rate = 0.06
 						self.czech.kick = self.stat_info.kick_tables.even_recoil
 						self.czech.kick_pattern = {
@@ -16724,17 +16724,15 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 			if self.gsh18 then
 				self.gsh18.recategorize = { "light_pis" }
 				self.gsh18.categories = {
-					"pistol",
-					"pistol_pdw"
+					"pistol"
 				}
 				self.gsh18.fire_mode_data.fire_rate =  0.1
-				self.gsh18.AMMO_MAX = 90
+				self.gsh18.AMMO_MAX = 75
 				self.gsh18.CLIP_AMMO_MAX = 18
-				self.gsh18.armor_piercing_chance = 0.75
-				self.gsh18.hs_mult = 1.2
+				self.gsh18.armor_piercing_chance = 0.5
 				self.gsh18.tactical_reload = 1
 				self.gsh18.has_description = true
-				self.gsh18.desc_id = "bm_sr1_sc_desc"
+				self.gsh18.desc_id = "bm_gsh18_sc_desc"
 				self.gsh18.lock_slide = true
 				self.gsh18.kick = self.stat_info.kick_tables.even_recoil
 				self.gsh18.kick_pattern = {
@@ -16748,10 +16746,10 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.gsh18.damage_falloff = {
 					start_dist = 1100,
 					end_dist = 3000,
-					min_mult = 0.3
+					min_mult = 0.25
 				}
 				self.gsh18.stats = {
-					damage = 20,
+					damage = 24,
 					spread = 58,
 					recoil = 83,
 					spread_moving = 9,
@@ -16771,8 +16769,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 					self.x_gsh18.recategorize = {"light_pis"}
 					self.x_gsh18.categories = {
 						"akimbo",
-						"pistol",
-						"pistol_pdw"
+						"pistol"
 					}
 					self.x_gsh18.fire_mode_data.fire_rate =  0.1
 					self.x_gsh18.BURST_FIRE = {
@@ -16782,13 +16779,12 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 						recoil_mult = 0.25,
 						last_recoil_mult = 1.05,
 					}
-					self.x_gsh18.AMMO_MAX = 180
+					self.x_gsh18.AMMO_MAX = 150
 					self.x_gsh18.CLIP_AMMO_MAX = 36
-					self.x_gsh18.armor_piercing_chance = 0.75
-					self.x_gsh18.hs_mult = 1.2
+					self.x_gsh18.armor_piercing_chance = 0.5
 					self.x_gsh18.tactical_reload = 2
 					self.x_gsh18.has_description = true
-					self.x_gsh18.desc_id = "bm_sr1_sc_desc"
+					self.x_gsh18.desc_id = "bm_gsh18_sc_desc"
 					self.x_gsh18.lock_slide = true
 					self.x_gsh18.kick = self.stat_info.kick_tables.even_recoil
 					self.x_gsh18.kick_pattern = {
@@ -16802,10 +16798,10 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 					self.x_gsh18.damage_falloff = {
 						start_dist = 1100,
 						end_dist = 3000,
-						min_mult = 0.3
+						min_mult = 0.25
 					}
 					self.x_gsh18.stats = {
-						damage = 20,
+						damage = 24,
 						spread = 48,
 						recoil = 73,
 						spread_moving = 9,
@@ -17091,40 +17087,40 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 
 			if self.virtus then
 				self.virtus.nato = true
-				self.virtus.recategorize = { "light_ar" }
+				self.virtus.recategorize = { "heavy_ar" }
 				self.virtus.damage_type = "assault_rifle"
 				self.virtus.has_description = false
 				self.virtus.tactical_reload = 1
-				self.virtus.AMMO_MAX = 150
+				self.virtus.AMMO_MAX = 120
 				self.virtus.fire_mode_data.fire_rate = 0.075
-				self.virtus.kick = self.stat_info.kick_tables.moderate_kick
+				self.virtus.kick = self.stat_info.kick_tables.moderate_right_kick
 				self.virtus.kick_pattern = {
-					{0, self.stat_info.kick_tables.moderate_kick},
-					{5, self.stat_info.kick_tables.pattern_r1},
-					{6, self.stat_info.kick_tables.moderate_left_kick},
-					{10, self.stat_info.kick_tables.right_kick},
+					{0, self.stat_info.kick_tables.vertical_kick},
+					{3, self.stat_info.kick_tables.right_kick},
+					{4, self.stat_info.kick_tables.pattern_r1},
+					{6, self.stat_info.kick_tables.moderate_right_kick},
 					{12, self.stat_info.kick_tables.moderate_kick},
-					{19, self.stat_info.kick_tables.moderate_right_kick}
+					{17, self.stat_info.kick_tables.moderate_right_kick}
 				}
 				self.virtus.supported = true
-				self.virtus.ads_speed = 0.280
+				self.virtus.ads_speed = 0.320
 				self.virtus.damage_falloff = {
 					start_dist = 1800,
 					end_dist = 5500,
-					min_mult = 0.5
+					min_mult = 0.4
 				}
 				self.virtus.stats = {
-					damage = 24,
-					spread = 75,
-					recoil = 85,
+					damage = 30,
+					spread = 70,
+					recoil = 83,
 					spread_moving = 6,
 					zoom = 1,
-					concealment = 27,
+					concealment = 23,
 					suppression = 8,
 					alert_size = 2,
 					extra_ammo = 101,
 					total_ammo_mod = 400,
-					value = 1,
+					value = 9,
 					reload = 20
 				}
 				self.virtus.stats_modifiers = nil
@@ -20810,7 +20806,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.einhander.damage_falloff = {
 					start_dist = 1200,
 					end_dist = 4000,
-					min_mult = 0.3
+					min_mult = 0.3333333
 				}
 				self.einhander.stats = {
 					damage = 18,
@@ -23755,7 +23751,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 			end
 
 			if self.lvoac then
-				self.lvoac.recategorize = { "heavy_ar" }
+				self.lvoac.recategorize = { "light_ar" }
 				self.lvoac.damage_type = "assault_rifle"
 				self.lvoac.tactical_reload = 1
 				self.lvoac.nato = true
@@ -23768,34 +23764,34 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.lvoac.fire_mode_data.fire_rate = 0.075
 				self.lvoac.CAN_TOGGLE_FIREMODE = true
 				self.lvoac.kick = {}
-				self.lvoac.kick = self.stat_info.kick_tables.moderate_right_kick
+				self.lvoac.kick = self.stat_info.kick_tables.moderate_kick
 				self.lvoac.kick_pattern = {
-					{0, self.stat_info.kick_tables.vertical_kick},
-					{3, self.stat_info.kick_tables.right_kick},
-					{4, self.stat_info.kick_tables.pattern_r1},
-					{6, self.stat_info.kick_tables.moderate_right_kick},
+					{0, self.stat_info.kick_tables.moderate_kick},
+					{5, self.stat_info.kick_tables.pattern_r1},
+					{6, self.stat_info.kick_tables.moderate_left_kick},
+					{10, self.stat_info.kick_tables.right_kick},
 					{12, self.stat_info.kick_tables.moderate_kick},
-					{17, self.stat_info.kick_tables.moderate_right_kick}
+					{19, self.stat_info.kick_tables.moderate_right_kick}
 				}
 				self.lvoac.supported = true
-				self.lvoac.ads_speed = 0.320
+				self.lvoac.ads_speed = 0.280
 				self.lvoac.damage_falloff = {
 					start_dist = 1800,
 					end_dist = 5500,
-					min_mult = 0.4
+					min_mult = 0.5
 				}
 				self.lvoac.stats = {
-					damage = 30,
-					spread = 72,
-					recoil = 83,
+					damage = 24,
+					spread = 76,
+					recoil = 87,
 					spread_moving = 6,
 					zoom = 1,
-					concealment = 23,
+					concealment = 27,
 					suppression = 8,
 					alert_size = 2,
 					extra_ammo = 101,
 					total_ammo_mod = 400,
-					value = 9,
+					value = 1,
 					reload = 20
 				}
 				self.lvoac.stats_modifiers = nil
@@ -28372,6 +28368,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 			if self.s556 then
 				self.s556.recategorize = { "heavy_ar" }
 				self.s556.damage_type = "assault_rifle"
+				self.s556.nato = true
 				self.s556.AMMO_MAX = 120
 				self.s556.tactical_reload = 1
 				self.s556.CLIP_AMMO_MAX = 30
@@ -28387,6 +28384,14 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 					burst_default = true
 				}
 				self.s556.kick = self.stat_info.kick_tables.moderate_right_kick
+				self.s556.kick_pattern = {
+					{0, self.stat_info.kick_tables.right_kick},
+					{3, self.stat_info.kick_tables.even_recoil},
+					{9, self.stat_info.kick_tables.moderate_right_kick},
+					{15, self.stat_info.kick_tables.moderate_left_kick},
+					{24, self.stat_info.kick_tables.vertical_kick},
+					{27, self.stat_info.kick_tables.left_recoil},
+				}
 				self.s556.supported = true
 				self.s556.ads_speed = 0.280
 				self.s556.damage_falloff = {
@@ -28427,6 +28432,13 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				self.ar47.CAN_TOGGLE_FIREMODE = true
 				self.ar47.BURST_FIRE = false
 				self.ar47.kick = self.stat_info.kick_tables.moderate_right_kick
+				self.ar47.kick_pattern = {
+					{0, self.stat_info.kick_tables.left_recoil},
+					{6, self.stat_info.kick_tables.moderate_kick},
+					{14, self.stat_info.kick_tables.moderate_left_kick},
+					{20, self.stat_info.kick_tables.moderate_kick},
+					{24, self.stat_info.kick_tables.moderate_right_kick}
+				}
 				self.ar47.supported = true
 				self.ar47.ads_speed = 0.300
 				self.ar47.damage_falloff = {
@@ -33640,7 +33652,7 @@ Hooks:PostHook( WeaponTweakData, "init", "SC_weapons", function(self)
 				weap.force_shake = true
 			end
 
-			if table.contains(weap.categories, "dmr_l") or table.contains(weap.categories, "dmr_h") then
+			if table.contains(weap.categories, "dmr_l") or table.contains(weap.categories, "dmr_h") and weap.CAN_TOGGLE_FIREMODE then
 				weap.SINGLE_FIRE_AP_ADD = 0.25
 			end
 
